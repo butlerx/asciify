de02e8ccee04230e4d20e119fc40b48b0a0dd102
butlerx <butlerx@notthe.cloud>
2018-09-16 12:25:47 +0100

    trim extra whitespace from the start and end of images

--- internal/pkg/asciify/readme.go
+++ internal/pkg/asciify/readme.go
@@ -1,12 +1,14 @@
 package asciify
 
 import (
+	"bufio"
 	"html/template"
 	"os"
+	"strings"
 )
 
 // Readme template
-type Readme struct {
+type readme struct {
 	Image string
 }
 
@@ -18,5 +20,36 @@ func InjectReadMe(readmePath, img string) error {
 		return err
 	}
 	defer output.Close()
-	return tmpl.Execute(output, Readme{"```\n" + img + "```\n"})
+	return tmpl.Execute(output, readme{wrapCode(img)})
+}
+
+func wrapCode(code string) string {
+	return "\n```\n" + trimWhitespace(code) + "\n```\n"
+}
+
+func trimWhitespace(txt string) string {
+	var formatted []string
+	scanner := bufio.NewScanner(strings.NewReader(txt))
+	for scanner.Scan() {
+		formatted = append(formatted, strings.TrimRight(scanner.Text(), " "))
+	}
+	return strings.Join(trimLeft(trimRight(formatted)), "\n")
+}
+
+func trimLeft(sa []string) []string {
+	for i := 0; i < len(sa); i++ {
+		if sa[i] != "" {
+			return sa[i:]
+		}
+	}
+	return sa
+}
+
+func trimRight(sa []string) []string {
+	for i := len(sa) - 1; i >= 0; i-- {
+		if sa[i] != "" {
+			return sa[0 : i+1]
+		}
+	}
+	return sa
 }
